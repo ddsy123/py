a34325f991cd4650ea5927d93438439dc3d5a65d
DDS <110591485+ddsy123@users.noreply.github.com>
2024-07-15 11:46:13 +0530

    Add files via upload

---
+++ py1/aa.py
@@ -0,0 +1,6 @@
+i = 1  
+char = 97  
+while i <= 5:
+    print(chr(char) * i) 
+    i += 1  
+    char += 1  
---
+++ py1/bb.py
@@ -0,0 +1,8 @@
+n = 3
+while n <= 15:
+    i = 3
+    while i <= n:
+        print(i,end=" ")
+        i += 3
+    print(" ")
+    n += 2
